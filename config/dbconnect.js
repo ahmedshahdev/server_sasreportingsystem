f46e9e96d2d2dc04dd0765bbd52f1338ced17859
Syed <sars.latest@gmail.com>
2023-11-22 19:17:55 +0400

    commit 7:17AM 11/12/2023 approved

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -5,8 +5,7 @@ mongoose.set('strictQuery', false)
 
 const connectToMongo = () => {
     try {
-        mongoose.connect(process.env.MONGODB_URI, {
-        })
+        mongoose.connect(process.env.MONGODB_URI, {})
         console.log("Connected successfully")
     } catch (err) {
         console.error("Error connecting db with server")
