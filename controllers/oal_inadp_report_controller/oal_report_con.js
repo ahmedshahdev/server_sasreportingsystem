aef3ac5344570baee531ce76aaaf980c93af48fe
Syed <sars.latest@gmail.com>
2023-11-30 20:05:00 +0400

    RENAME ALL OAL INAD REPORT PROPERLY

--- controllers/oal_inadp_report_controller/oal_report_con.js
+++
@@ -1,8 +0,0 @@
-const add_report_con = require("./add_oal_report_con")
-const fetchall_oal_report_con = require("./fetchall_oal_report_con")
-const update_oal_report_con = require("./update_oal_report_con")
-const delete_oal_report_con = require("./delate_oal_report_con copy")
-
-
-// module.exports = {add_report_con, view_oal_report_con};
-module.exports = {add_report_con, fetchall_oal_report_con, update_oal_report_con, delete_oal_report_con};
